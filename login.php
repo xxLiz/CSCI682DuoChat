572afc2c06b850b9d56986620b2273dd9cc81602
kbotova <89753557+kbotova@users.noreply.github.com>
2024-10-03 12:44:12 -0500

    Style is corrected, footer and nav are added

--- login.php
+++ login.php
@@ -6,22 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <title>LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="test1.css" />
+    <title>DuoChat Login</title>
     <script src="https://code.jquery.com/jquery-3.3.1.js"
         integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous">
-        </script>
-    <script>
-        $(function () {
-            $("#header").load("header.php");
-            $("#footer").load("footer.html");
-        });
     </script>
 </head>
 
-<body>
-    <div id="header"></div>
-    <div class="container">
+<body class="registration-login-page">
+    <nav>
+        <h1 class="end">DuoChat App</h1>
+        <div class="nav-items">
+            <span class="background" id="background"></span>
+            <a href="#profile">
+                <span class="material-symbols-outlined">person</span>
+            </a>
+        </div>
+    </nav>
+    <div class="registration-login-container">
         <!-- Display session message if it exists -->
         <?php
         session_start();
@@ -36,22 +38,24 @@
             unset($_SESSION['success-message']);
         }
         ?>
-        <h2 class="text-center">LOGIN</h2>
-        <form action="handle_login.php" method="post">
-            <div class="form-group">
+        <form class="registration-login-form" action="handle_login.php" method="post">
+		<h2 class="registration-login-title">Login</h2>
+            <div>
                 <label for="email">Email:</label>
-                <input type="text" class="form-control" placeholder="Enter your Email" id="email" name="email" required>
+                <input type="text" placeholder="Enter Email" id="email" name="email" required>
             </div>
-            <div class="form-group">
+            <div>
                 <label for="password">Password:</label>
-                <input type="password" class="form-control" placeholder="Enter your Password" id="password"
+                <input type="password" placeholder="Enter Password" id="password"
                     name="password" required>
             </div>
-            <input type="submit" value="Login">
-        </form>
-        <p style="text-align: center;">Not Yet Registered? <a href="handle_registration.php">Sign Up</a></p>
+            <input class="register-login-button" type="submit" value="Login">
+			<p class="registration-login-text">Not Yet Registered? <a href="handle_registration.php">Sign Up</a></p>
+		</form>
     </div>
-    <div id="footer"></div>
+	<footer>
+        <p>&copy; 2024 DuoChat App. All rights reserved.</p>
+    </footer>
     <script>
         // Automatically hide error message after 5 seconds
         setTimeout(function () {
